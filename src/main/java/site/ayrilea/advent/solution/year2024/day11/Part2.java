899735047a0fc2c7cf6f700eb265268796a22847
ayrilea <ayrilea.code@gmail.com>
2024-12-10 17:34:14 +1100

    Add Day 11 skeleton

---
+++ src/main/java/site/ayrilea/advent/solution/year2024/day11/Part2.java
@@ -0,0 +1,14 @@
+package site.ayrilea.advent.solution.year2024.day11;
+
+import site.ayrilea.advent.input.Input;
+import site.ayrilea.advent.solution.Solution;
+import site.ayrilea.advent.solution.SolutionMetadata;
+
+@SolutionMetadata(year = 2024, day = 11, part = 1)
+public class Part2 implements Solution<Integer> {
+
+    @Override
+    public Integer solve(Input input) {
+        return 0;
+    }
+}
