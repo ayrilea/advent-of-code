d42b94af2f4f2dda3874c3220231bcc258a3f438
ayrilea <ayrilea.code@gmail.com>
2024-12-11 22:12:17 +1100

    Add Day 12 skeleton

---
+++ src/main/java/site/ayrilea/advent/solution/year2024/day12/Part1.java
@@ -0,0 +1,14 @@
+package site.ayrilea.advent.solution.year2024.day12;
+
+import site.ayrilea.advent.input.Input;
+import site.ayrilea.advent.solution.Solution;
+import site.ayrilea.advent.solution.SolutionMetadata;
+
+@SolutionMetadata(year = 2024, day = 12, part = 1)
+public class Part1 implements Solution<Long> {
+
+    @Override
+    public Long solve(Input input) {
+        return 0L;
+    }
+}
---
+++ src/test/java/site/ayrilea/advent/solution/year2024/day12/Part1Test.java
@@ -0,0 +1,19 @@
+package site.ayrilea.advent.solution.year2024.day12;
+
+import org.junit.jupiter.api.DisplayName;
+import site.ayrilea.advent.solution.AbstractSolutionTest;
+import site.ayrilea.advent.solution.Solution;
+
+@DisplayName("Year 2024, Day 12, Part 1")
+public class Part1Test extends AbstractSolutionTest<Long> {
+
+    @Override
+    protected Solution<Long> createSolution() {
+        return new Part1();
+    }
+
+    @Override
+    protected Long exampleExpectedValue() {
+        return 0L;
+    }
+}
