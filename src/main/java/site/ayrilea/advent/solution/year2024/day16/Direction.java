3fb35e7e9488873782426a0d52cdef512b57b589
ayrilea <ayrilea.code@gmail.com>
2024-12-21 21:42:03 +1100

    Refactor Day 16 Part 1 solution to add direction movement into enum and avoid duplicated cases checking in main loop

--- src/main/java/site/ayrilea/advent/solution/year2024/day16/Direction.java
+++ src/main/java/site/ayrilea/advent/solution/year2024/day16/Direction.java
@@ -2,8 +2,18 @@ package site.ayrilea.advent.solution.year2024.day16;
 
 public enum Direction {
 
-    EAST,
-    NORTH,
-    SOUTH,
-    WEST;
+    EAST(new Position(0, 1)),
+    NORTH(new Position(-1, 0)),
+    SOUTH(new Position(1, 0)),
+    WEST(new Position(0, -1));
+
+    private final Position modifier;
+
+    Direction(Position modifier) {
+        this.modifier = modifier;
+    }
+
+    Position move(Position current) {
+        return new Position(current.row() + modifier.row(), current.column() + modifier.column());
+    }
 }
--- src/main/java/site/ayrilea/advent/solution/year2024/day16/Part1.java
+++ src/main/java/site/ayrilea/advent/solution/year2024/day16/Part1.java
@@ -6,8 +6,6 @@ import site.ayrilea.advent.solution.SolutionMetadata;
 
 import java.util.*;
 
-import static site.ayrilea.advent.solution.year2024.day16.Direction.*;
-
 @SolutionMetadata(year = 2024, day = 16, part = 1)
 public class Part1 implements Solution<Integer> {
 
@@ -26,35 +24,13 @@ public class Part1 implements Solution<Integer> {
             current = unvisited.remove();
             visited.add(current);
 
-            int column = current.getPosition().column();
-            int row = current.getPosition().row();
-
-            Position up = new Position(row - 1, column);
-            if (!walls.contains(up) && isNotVisited(visited, up, NORTH)) {
-                unvisited.add(new Node.Builder(up, NORTH)
-                        .lengthFrom(current)
-                        .build());
-            }
-
-            Position down = new Position(row + 1, column);
-            if (!walls.contains(down) && isNotVisited(visited, down, SOUTH)) {
-                unvisited.add(new Node.Builder(down, SOUTH)
-                        .lengthFrom(current)
-                        .build());
-            }
-
-            Position left = new Position(row, column - 1);
-            if (!walls.contains(left) && isNotVisited(visited, left, EAST)) {
-                unvisited.add(new Node.Builder(left, EAST)
-                        .lengthFrom(current)
-                        .build());
-            }
-
-            Position right = new Position(row, column + 1);
-            if (!walls.contains(right) && isNotVisited(visited, right, WEST)) {
-                unvisited.add(new Node.Builder(right, WEST)
-                        .lengthFrom(current)
-                        .build());
+            for (Direction direction : Direction.values()) {
+                Position position = direction.move(current.getPosition());
+                if (!walls.contains(position) && isNotVisited(visited, position, direction)) {
+                    unvisited.add(new Node.Builder(position, direction)
+                            .lengthFrom(current)
+                            .build());
+                }
             }
         }
         return current.getLength();
