6bfd2fc84e0fc953a8200cbed2e9b968c00b4cf9
ayrilea <ayrilea.code@gmail.com>
2024-12-22 20:39:30 +1100

    Cleanup Day 18

--- src/main/java/site/ayrilea/advent/solution/year2024/day18/Node.java
+++ src/main/java/site/ayrilea/advent/solution/year2024/day18/Node.java
@@ -1,17 +1,14 @@
 package site.ayrilea.advent.solution.year2024.day18;
 
-import java.util.List;
 import java.util.Objects;
 
 class Node implements Comparable<Node> {
 
     private final int length;
-    //private final List<Position> path;
     private final Position position;
 
-    private Node(int length, List<Position> path, Position position) {
+    private Node(int length, Position position) {
         this.length = length;
-        //this.path = path;
         this.position = position;
     }
 
@@ -38,22 +35,16 @@ class Node implements Comparable<Node> {
 
     static Node fromNode(Node previous, Position position) {
         int length = previous.getLength() + 1;
-//        List<Position> path = new LinkedList<>(previous.getPath());
-//        path.add(position);
-        return new Node(length, null, position);
+        return new Node(length, position);
     }
 
     static Node initialNode(Position position) {
-        return new Node(0, List.of(position), position);
+        return new Node(0, position);
     }
 
     int getLength() {
         return length;
     }
-
-//    List<Position> getPath() {
-//        return path;
-//    }
 
     Position getPosition() {
         return position;
