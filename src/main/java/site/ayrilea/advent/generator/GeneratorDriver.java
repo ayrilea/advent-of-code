d36dfc5cc3d7b7556ba4f10fd7bdec327e188987
ayrilea <ayrilea.code@gmail.com>
2025-11-30 12:08:22 +1100

    Refactor GeneratorDriver
    Argument parsing extracted into separate helper method

--- src/main/java/site/ayrilea/advent/generator/GeneratorDriver.java
+++ src/main/java/site/ayrilea/advent/generator/GeneratorDriver.java
@@ -3,22 +3,22 @@ package site.ayrilea.advent.generator;
 public class GeneratorDriver {
 
     static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java Generator <year>");
-            System.err.println("Example: java Generator 2025");
-            System.exit(1);
-        }
-
-        int year;
-        try {
-            year = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            System.err.println("Year must be a valid number");
-            System.exit(1);
-            return;
-        }
+        int year = parseArgs(args);
 
         new Generator(year).generate();
         System.out.println("Advent of Code " + year + " skeleton generated successfully!");
+    }
+
+    private static int parseArgs(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Invalid generator arguments. " +
+                    "Single argument representing year is required.");
+        }
+
+        try {
+            return Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Year must be a valid number.");
+        }
     }
 }
