96a7bdda3787c45bc9b93b8a002baa1f73788814
ayrilea <ayrilea.code@gmail.com>
2025-11-30 12:03:41 +1100

    Refactor generator
    Split writing and generation logic into separate methods, and combined generating/writing classes together, and input files together.
    
    Moved templates to separate files which are read in by Generator on instance creation to keep code cleaner

--- src/main/java/site/ayrilea/advent/generator/Generator.java
+++ src/main/java/site/ayrilea/advent/generator/Generator.java
@@ -1,28 +1,36 @@
 package site.ayrilea.advent.generator;
 
+import site.ayrilea.advent.input.FileInputException;
+
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Generator {
 
-    private static final String SOLUTION_PACKAGE = "site.ayrilea.advent.solution";
+    private static final String SOLUTION_PACKAGE_FORMAT = "site.ayrilea.advent.solution.year%d.day%d";
 
     private final Path projectRoot;
+    private final String templateSolution;
+    private final String templateTest;
     private final int year;
 
     public Generator(int year) {
-        this.projectRoot = findProjectRoot();
         this.year = year;
+
+        projectRoot = findProjectRoot();
+        templateSolution = readTemplate("Solution.java.template");
+        templateTest = readTemplate("SolutionTest.java.template");
     }
 
     public void generate() {
         try {
-            generateSolutionClasses();
-            generateTestClasses();
-            generateMainInputFiles();
-            generateTestInputFiles();
+            generateClassFiles();
+            generateInputFiles();
         } catch (IOException e) {
             throw new RuntimeException("Failed to generate skeleton for year " + year, e);
         }
@@ -41,113 +49,77 @@ public class Generator {
         throw new IllegalStateException("Could not find project root (no build.gradle, pom.xml or gradlew found)");
     }
 
-    private void generateSolutionClasses() throws IOException {
-        Path basePath = projectRoot.resolve(Paths.get("src", "main", "java", "site", "ayrilea", "advent",
+    private void generateClassFiles() throws IOException {
+        Path mainSrc = projectRoot.resolve(Paths.get("src", "main", "java", "site", "ayrilea", "advent",
+                "solution", "year" + year));
+        Path testSrc = projectRoot.resolve(Paths.get("src", "test", "java", "site", "ayrilea", "advent",
                 "solution", "year" + year));
 
-        for (int day = 1; day <= 25; day++) {
-            Path dayPackage = basePath.resolve("day" + day);
-            Files.createDirectories(dayPackage);
+        Files.createDirectories(mainSrc);
+        Files.createDirectories(testSrc);
 
+        for (int day = 1; day <= 25; day++) {
+            Path daySolution = mainSrc.resolve("day" + day);
+            Path dayTest = testSrc.resolve("day" + day);
+
+            Files.createDirectories(daySolution);
+            Files.createDirectories(dayTest);
+
+            String packageName = SOLUTION_PACKAGE_FORMAT.formatted(year, day);
             for (int part = 1; part <= 2; part++) {
-                String className = "Part" + part;
-                String packageName = SOLUTION_PACKAGE + ".year" + year + ".day" + day;
+                String solutionClassName = "Part" + part;
+                String testClassName = solutionClassName + "Test";
 
-                String content = """
-                    package %s;
+                String solutionClass = templateSolution.formatted(packageName, year, day, part, solutionClassName);
+                String testClass = templateTest.formatted(packageName, year, day, part, testClassName, solutionClassName);
 
-                    import site.ayrilea.advent.input.Input;
-                    import site.ayrilea.advent.solution.Solution;
-                    import site.ayrilea.advent.solution.SolutionMetadata;
-
-                    @SolutionMetadata(year = %d, day = %d, part = %d)
-                    public class %s implements Solution<Integer> {
-
-                        @Override
-                        public Integer solve(Input input) {
-                            return 0;
-                        }
-                    }
-                    """.formatted(packageName, year, day, part, className);
-
-                Path file = dayPackage.resolve(className + ".java");
-                Files.writeString(file, content.stripTrailing());
-                System.out.println("Created solution class file: " + file);
+                writeClassFile(daySolution, solutionClassName, solutionClass);
+                writeClassFile(dayTest, testClassName, testClass);
             }
         }
     }
 
-    private void generateTestClasses() throws IOException {
-        Path basePath = projectRoot.resolve(Paths.get("src", "test", "java", "site", "ayrilea",
-                "advent", "solution", "year" + year));
-
-        for (int day = 1; day <= 25; day++) {
-            Path dayPackage = basePath.resolve("day" + day);
-            Files.createDirectories(dayPackage);
-
-            for (int part = 1; part <= 2; part++) {
-                String partClass = "Part" + part;
-                String testClassName = partClass + "Test";
-                String packageName = SOLUTION_PACKAGE + ".year" + year + ".day" + day;
-
-                String content = """
-                    package %s;
-
-                    import org.junit.jupiter.api.DisplayName;
-                    import site.ayrilea.advent.solution.AbstractSolutionTest;
-                    import site.ayrilea.advent.solution.Solution;
-
-                    @DisplayName("Year %d, Day %d, Part %d")
-                    public class %s extends AbstractSolutionTest<Integer> {
-
-                        @Override
-                        protected Solution<Integer> createSolution() {
-                            return new %s();
-                        }
-
-                        @Override
-                        protected Integer exampleExpectedValue() {
-                            return 0;
-                        }
-                    }
-                    """.formatted(packageName, year, day, part, testClassName, partClass);
-
-                Path file = dayPackage.resolve(testClassName + ".java");
-                Files.writeString(file, content.stripTrailing());
-                System.out.println("Created solution test class file: " + file);
-            }
-        }
-    }
-
-    private void generateMainInputFiles() throws IOException {
-        Path basePath = projectRoot.resolve(Paths.get("src", "main", "resources", "site", "ayrilea",
+    private void generateInputFiles() throws IOException {
+        Path mainResources = projectRoot.resolve(Paths.get("src", "main", "resources", "site", "ayrilea",
                 "advent", "input", "year" + year));
-        Files.createDirectories(basePath);
+        Path testResources = projectRoot.resolve(Paths.get("src", "test", "resources", "site", "ayrilea",
+                "advent", "input", "year" + year));
+
+        Files.createDirectories(mainResources);
+        Files.createDirectories(testResources);
 
         for (int day = 1; day <= 25; day++) {
-            Path file = basePath.resolve("day" + day + ".txt");
-            if (!Files.exists(file)) {
-                Files.createFile(file);
-                System.out.println("Created problem input file: " + file);
-            } else {
-                System.out.println("Skipped problem input file (already exists): " + file);
-            }
+            writeInputFile(mainResources, day);
+            writeInputFile(testResources, day);
         }
     }
 
-    private void generateTestInputFiles() throws IOException {
-        Path basePath = projectRoot.resolve(Paths.get("src", "test", "resources", "site", "ayrilea",
-                "advent", "input", "year" + year));
-        Files.createDirectories(basePath);
+    private String readTemplate(String filename) {
+        URL resource = Generator.class.getResource(filename);
+        if (resource == null) {
+            throw new FileInputException(filename + " does not exist");
+        }
+        try {
+            Path path = Paths.get(resource.toURI());
+            return Files.readString(path, StandardCharsets.UTF_8);
+        } catch (IOException | URISyntaxException e) {
+            throw new IllegalStateException("Failed to retrieve path for " + resource, e);
+        }
+    }
 
-        for (int day = 1; day <= 25; day++) {
-            Path file = basePath.resolve("day" + day + ".txt");
-            if (!Files.exists(file)) {
-                Files.createFile(file);
-                System.out.println("Created test input file: " + file);
-            } else {
-                System.out.println("Skipped test input file (already exists): " + file);
-            }
+    private void writeClassFile(Path basePath, String className, String content) throws IOException {
+        Path file = basePath.resolve(className + ".java");
+        Files.writeString(file, content.stripTrailing());
+        System.out.println("Created class file: " + file);
+    }
+
+    private void writeInputFile(Path basePath, int day) throws IOException {
+        Path file = basePath.resolve("day" + day + ".txt");
+        if (!Files.exists(file)) {
+            Files.createFile(file);
+            System.out.println("Created input file: " + file);
+        } else {
+            System.out.println("Skipped input file (already exists): " + file);
         }
     }
 }
