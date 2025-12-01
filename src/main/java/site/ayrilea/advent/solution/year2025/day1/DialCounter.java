9e4bf2f0179b5cbaaee0b37cec65e02e891d94de
ayrilea <ayrilea.code@gmail.com>
2025-12-01 17:25:18 +1100

    Refactor processing into Gatherer
    Updated Part1 and Part2 to use shared processing via a includePasses flag

---
+++ src/main/java/site/ayrilea/advent/solution/year2025/day1/DialCounter.java
@@ -0,0 +1,69 @@
+package site.ayrilea.advent.solution.year2025.day1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+import java.util.stream.Gatherer;
+
+record DialCounter(int initial, boolean includePasses) implements Gatherer<Integer, List<Integer>, Integer> {
+
+    @Override
+    public BiConsumer<List<Integer>, Downstream<? super Integer>> finisher() {
+        return (state, downstream) -> {
+            downstream.push(state.get(1));
+        };
+    }
+
+    @Override
+    public Integrator<List<Integer>, Integer, Integer> integrator() {
+        return Integrator.of(
+                (state, rotation, _) -> {
+                    int value = state.get(0);
+                    int count = state.get(1);
+
+                    int originalValue = value;
+                    int fullRotations = rotation / 100;
+                    int netRotation = rotation % 100;
+
+                    value += netRotation;
+                    if (value == 100) {
+                        value = 0;
+                    }
+                    if (value > 99) {
+                        value -= 100;
+                        if (includePasses) {
+                            count++;
+                        }
+                    }
+                    if (value < 0) {
+                        value += 100;
+                        if (originalValue != 0) {
+                            if (includePasses) {
+                                count++;
+                            }
+                        }
+                    }
+
+                    if (value == 0) {
+                        count++;
+                    }
+                    if (includePasses) {
+                        count += Math.abs(fullRotations);
+                    }
+
+                    state.set(0, value);
+                    state.set(1, count);
+                    return true;
+                }
+        );
+    }
+
+    @Override
+    public Supplier<List<Integer>> initializer() {
+        List<Integer> state = new ArrayList<>(2);
+        state.add(initial);
+        state.add(0);
+        return () -> state;
+    }
+}
--- src/main/java/site/ayrilea/advent/solution/year2025/day1/Part2.java
+++ src/main/java/site/ayrilea/advent/solution/year2025/day1/Part2.java
@@ -4,46 +4,15 @@ import site.ayrilea.advent.input.Input;
 import site.ayrilea.advent.solution.Solution;
 import site.ayrilea.advent.solution.SolutionMetadata;
 
-import java.util.List;
-
 @SolutionMetadata(year = 2025, day = 1, part = 2)
 public class Part2 implements Solution<Integer> {
 
     @Override
     public Integer solve(Input input) {
-        int count = 0;
-        List<Integer> rotations = input.stream().map(Shared::parseLine).toList();
-
-        int value = 50;
-        System.out.println("Value=[" + value + "]  Count=[" + count + "]");
-        for (int rotation : rotations) {
-            int originalValue = value;
-            int fullRotations = rotation / 100;
-            int netRotation = rotation % 100;
-
-            value += netRotation;
-            if (value == 100) {
-                value = 0;
-            }
-            if (value > 99) {
-                value -= 100;
-                count++;
-            }
-            if (value < 0) {
-                value += 100;
-                if (originalValue != 0) {
-                    count++;
-                }
-            }
-
-            if (value == 0) {
-                count++;
-            }
-            count += Math.abs(fullRotations);
-
-            System.out.println("Value=[" + value + "]  Count=[" + count + "]");
-        }
-
-        return count;
+        return input.stream()
+                .map(Shared::parseLine)
+                .gather(new DialCounter(50, true))
+                .findFirst()
+                .orElseThrow();
     }
 }
