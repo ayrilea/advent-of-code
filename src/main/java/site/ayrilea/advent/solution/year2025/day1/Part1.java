196f44287aa2a2eebf2e3abdae9280e82896ee8f
ayrilea <ayrilea.code@gmail.com>
2025-12-01 16:49:51 +1100

    Add 2025 Day 1 solutions
    Not yet fully refactored. Still need to think about the maths around Part 2 and see if it can be done cleaner.

--- src/main/java/site/ayrilea/advent/solution/year2025/day1/Part1.java
+++ src/main/java/site/ayrilea/advent/solution/year2025/day1/Part1.java
@@ -4,11 +4,30 @@ import site.ayrilea.advent.input.Input;
 import site.ayrilea.advent.solution.Solution;
 import site.ayrilea.advent.solution.SolutionMetadata;
 
+import java.util.List;
+
 @SolutionMetadata(year = 2025, day = 1, part = 1)
 public class Part1 implements Solution<Integer> {
 
     @Override
     public Integer solve(Input input) {
-        return 0;
+        int count = 0;
+        List<Integer> rotations = input.stream().map(Shared::parseLine).toList();
+
+        int value = 50;
+        for (int rotation : rotations) {
+            value += rotation;
+            while (value > 99) {
+                value = value - 100;
+            }
+            while (value < 0) {
+                value = value + 100;
+            }
+            if (value == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
---
+++ src/main/java/site/ayrilea/advent/solution/year2025/day1/Shared.java
@@ -0,0 +1,27 @@
+package site.ayrilea.advent.solution.year2025.day1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Shared {
+
+    private static final Pattern PATTERN_ROTATION = Pattern.compile("(?<direction>[LR])(?<amount>\\d{1,3})");
+
+    static int parseDirectionMultiplier(String direction) {
+        return switch (direction) {
+            case "L" -> -1;
+            case "R" -> 1;
+            default -> throw new IllegalArgumentException("Invalid direction: " + direction);
+        };
+    }
+
+    static int parseLine(String line) {
+        Matcher matcher = PATTERN_ROTATION.matcher(line);
+        if (matcher.matches()) {
+            int directionMultiplier = parseDirectionMultiplier(matcher.group("direction"));
+            int amount = Integer.parseInt(matcher.group("amount"));
+            return directionMultiplier * amount;
+        }
+        throw new IllegalArgumentException("Invalid input line: " + line);
+    }
+}
