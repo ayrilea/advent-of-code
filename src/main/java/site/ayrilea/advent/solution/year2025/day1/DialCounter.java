1268eea0eac221bee33da64e067b270e42c0c309
ayrilea <ayrilea.code@gmail.com>
2025-12-02 11:24:21 +1100

    Add comments explaining DialCounter algorithm

--- src/main/java/site/ayrilea/advent/solution/year2025/day1/DialCounter.java
+++ src/main/java/site/ayrilea/advent/solution/year2025/day1/DialCounter.java
@@ -11,6 +11,7 @@ record DialCounter(int initial, boolean includePasses) implements Gatherer<Integ
     @Override
     public BiConsumer<List<Integer>, Downstream<? super Integer>> finisher() {
         return (state, downstream) -> {
+            //Push the count downstream
             downstream.push(state.get(1));
         };
     }
@@ -26,28 +27,37 @@ record DialCounter(int initial, boolean includePasses) implements Gatherer<Integ
                     int fullRotations = rotation / 100;
                     int netRotation = rotation % 100;
 
+                    //netRotations is in the range [-99,99], and value is in the range [0,99]
+                    //This will leave value in the range [-99,198]
                     value += netRotation;
-                    if (value == 100) {
-                        value = 0;
+
+                    //First, count actually stopping at 0
+                    //This includes 100 (since the netRotation allows for +100 exclusive either way around)
+                    if (value == 0 || value == 100) {
+                        value = 0; //Set proper position here
+                        count++;
                     }
+
+                    //Next, adjust for if we're outside proper value range [0,99]
+                    //This will require either a single +100, a single -100, or nothing
                     if (value > 99) {
                         value -= 100;
+                        //We moved from [0,99] to [100,198] in the net rotation, which passed 0
                         if (includePasses) {
                             count++;
                         }
-                    }
-                    if (value < 0) {
+                    } else if (value < 0) {
                         value += 100;
+                        //We moved from [0,99] to [-99,-1] in the net rotation
                         if (originalValue != 0) {
+                            //This handles if we _started_ at zero (which is not another pass)
                             if (includePasses) {
                                 count++;
                             }
                         }
                     }
 
-                    if (value == 0) {
-                        count++;
-                    }
+                    //Count the full rotations as passing 0
                     if (includePasses) {
                         count += Math.abs(fullRotations);
                     }
