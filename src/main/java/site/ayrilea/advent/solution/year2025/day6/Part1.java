f1ab93ae4528a103f5d73e8a38130dcf7798ce36
ayrilea <ayrilea.code@gmail.com>
2025-12-06 16:10:29 +1100

    Change 2025 Day 6 to Long type solution
    Added example and main input, and example test value for Part 1

--- src/main/java/site/ayrilea/advent/solution/year2025/day6/Part1.java
+++ src/main/java/site/ayrilea/advent/solution/year2025/day6/Part1.java
@@ -5,10 +5,10 @@ import site.ayrilea.advent.solution.Solution;
 import site.ayrilea.advent.solution.SolutionMetadata;
 
 @SolutionMetadata(year = 2025, day = 6, part = 1)
-public class Part1 implements Solution<Integer> {
+public class Part1 implements Solution<Long> {
 
     @Override
-    public Integer solve(Input input) {
-        return 0;
+    public Long solve(Input input) {
+        return 0L;
     }
 }
