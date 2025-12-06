90aa05a096b15d712b4569966536789ea73bf6cb
ayrilea <ayrilea.code@gmail.com>
2025-12-06 21:47:20 +1100

    Remove unused import

--- src/main/java/site/ayrilea/advent/solution/year2025/day6/Part2.java
+++ src/main/java/site/ayrilea/advent/solution/year2025/day6/Part2.java
@@ -5,7 +5,6 @@ import site.ayrilea.advent.solution.Solution;
 import site.ayrilea.advent.solution.SolutionMetadata;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
