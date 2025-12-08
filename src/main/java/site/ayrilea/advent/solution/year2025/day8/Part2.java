30c8a60aa1c7c32fd63da895c80260c682f543eb
ayrilea <ayrilea.code@gmail.com>
2025-12-08 22:10:11 +1100

    Add solution for 2025 Day 8 Part 2

--- src/main/java/site/ayrilea/advent/solution/year2025/day8/Part2.java
+++ src/main/java/site/ayrilea/advent/solution/year2025/day8/Part2.java
@@ -4,11 +4,28 @@ import site.ayrilea.advent.input.Input;
 import site.ayrilea.advent.solution.Solution;
 import site.ayrilea.advent.solution.SolutionMetadata;
 
+import java.util.List;
+import java.util.Set;
+
+import static site.ayrilea.advent.solution.year2025.day8.Shared.*;
+import static site.ayrilea.advent.solution.year2025.day8.Shared.parseInput;
+
 @SolutionMetadata(year = 2025, day = 8, part = 2)
-public class Part2 implements Solution<Integer> {
+public class Part2 implements Solution<Long> {
 
     @Override
-    public Integer solve(Input input) {
-        return 0;
+    public Long solve(Input input) {
+        ParsedInput parsedInput = parseInput(input);
+
+        List<Pair> closestPairs = getClosestPairs(parsedInput, false);
+        List<Set<Junction>> circuits = getStartingCircuits(parsedInput);
+
+        Pair current;
+        do {
+            current = closestPairs.removeFirst();
+            connectPair(circuits, current);
+        } while (circuits.size() > 1);
+
+        return current.first().x() * current.second().x();
     }
 }
