c80e393c6cf95d382d3050942843b7c4f60f0e63
ayrilea <ayrilea.code@gmail.com>
2025-12-10 21:06:07 +1100

    Add solution for 2025 Day 10 Part 1

---
+++ src/main/java/site/ayrilea/advent/solution/year2025/day10/Machine.java
@@ -0,0 +1,48 @@
+package site.ayrilea.advent.solution.year2025.day10;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+record Machine(Boolean[] desiredState, List<Boolean[]> buttons) {
+
+    private static final Pattern PATTERN_MACHINE = Pattern.compile(
+            "\\[(?<desiredState>[.#]+)] (?<buttons>.+) \\{[\\d,]+}");
+
+    static Machine fromInput(String machine) {
+        Matcher matcher = PATTERN_MACHINE.matcher(machine);
+        if (matcher.matches()) {
+            Boolean[] desiredState = parseDesiredState(matcher.group("desiredState"));
+            List<Boolean[]> buttons = parseButtons(matcher.group("buttons"), desiredState.length);
+            return new Machine(desiredState, buttons);
+        }
+        throw new IllegalArgumentException("Invalid input machine: " + machine);
+    }
+
+    private static Boolean[] parseButton(String button, int numberOfLights) {
+        Boolean[] parsedButton = new Boolean[numberOfLights];
+        IntStream.range(0, numberOfLights).forEach(i -> parsedButton[i] = false);
+
+        Arrays.stream(button.substring(1, button.length() - 1)
+                .split(","))
+                .map(Integer::parseInt)
+                .forEach(i -> parsedButton[i] = true);
+
+        return parsedButton;
+    }
+
+    private static List<Boolean[]> parseButtons(String buttons, int numberOfLights) {
+        return Arrays.stream(buttons.split(" "))
+                .map(button -> parseButton(button, numberOfLights))
+                .toList();
+    }
+
+    private static Boolean[] parseDesiredState(String desiredState) {
+        return desiredState.chars()
+                .boxed()
+                .map(c -> c == '#')
+                .toArray(Boolean[]::new);
+    }
+}
--- src/main/java/site/ayrilea/advent/solution/year2025/day10/Part1.java
+++ src/main/java/site/ayrilea/advent/solution/year2025/day10/Part1.java
@@ -4,11 +4,72 @@ import site.ayrilea.advent.input.Input;
 import site.ayrilea.advent.solution.Solution;
 import site.ayrilea.advent.solution.SolutionMetadata;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 @SolutionMetadata(year = 2025, day = 10, part = 1)
 public class Part1 implements Solution<Integer> {
 
     @Override
     public Integer solve(Input input) {
-        return 0;
+        return input.stream()
+                .map(Machine::fromInput)
+                .map(Part1::calculateFewestButtonPressesNeeded)
+                .mapToInt(i -> i)
+                .sum();
+    }
+
+    private static boolean applyPresses(Machine machine, List<Integer> buttonPresses) {
+        Boolean[] state = new Boolean[machine.desiredState().length];
+        IntStream.range(0, state.length).forEach(i -> state[i] = false);
+        buttonPresses.forEach(buttonIndex -> {
+            Boolean[] button = machine.buttons().get(buttonIndex);
+            for (int i = 0; i < state.length; i++) {
+                state[i] = state[i] ^ button[i];
+            }
+        });
+
+        return Arrays.equals(state, machine.desiredState());
+    }
+
+    private static void backtrack(List<List<Integer>> result, List<Integer> current,
+                                  int nButtons, int remaining, int start) {
+        if (remaining == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        for (int i = start; i < nButtons; i++) {
+            current.add(i);
+            backtrack(result, current, nButtons, remaining - 1, i);
+            current.removeLast();
+        }
+    }
+
+    private static int calculateFewestButtonPressesNeeded(Machine machine) {
+        int buttonPresses = 1;
+        while (!canSolve(machine, buttonPresses)) {
+            buttonPresses++;
+        }
+
+        return buttonPresses;
+    }
+
+    private static boolean canSolve(Machine machine, int buttonPresses) {
+        List<List<Integer>> buttonCombinations = getButtonCombinations(machine.buttons().size(), buttonPresses);
+
+        for (var buttonCombination :buttonCombinations) {
+            if (applyPresses(machine, buttonCombination)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static List<List<Integer>> getButtonCombinations(int nButtons, int nPresses) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nButtons, nPresses, 0);
+        return result;
     }
 }
